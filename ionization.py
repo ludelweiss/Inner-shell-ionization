beced79d3579055eb026e962c963b5c9fbb3bd1d
Lud <aludmilla@orange.fr>
2022-11-03 14:32:55 +0100

    added two tables

--- ionization.py
+++ ionization.py
@@ -4,5 +4,9 @@
 Created on Thu Nov  3 13:49:33 2022
 
 @author: ludelweiss
+
+Modelling of the average number of electrons emitted following the decay of holes in an element
 """
+
+import numpy as np
 
