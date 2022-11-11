a59b29daa17c35fd8d308ba7da69cdd3e1fd10f4
Lud <aludmilla@orange.fr>
2022-11-11 10:50:38 +0100

    Graph was incorrect because the graph was for L_2 instead of M_1, now fixed. Also renamed some variables.

--- emitted_electrons.py
+++ emitted_electrons.py
@@ -27,7 +27,7 @@ stages = np.loadtxt("ionisation_stages", dtype = str)
 gaps = np.loadtxt("initial_gap", dtype = str)
 
 
-def correspondence(Z, st, s):
+def correspondence(Z, st, s):   # used to give the details of each ionisation in the graph legend/ title
     return elements[Z-1], stages[st-1], gaps[s-1]
 
 
@@ -38,11 +38,9 @@ def electrons(Z, st, s):
     
     if len(Z_idx) > 1 :
         st_idx = np.where(table[Z_idx[0]:Z_idx[len(Z_idx)-1], 1] == st)
-        
     elif (len(Z_idx) == 1):
-        st_idx = np.where(table[Z_idx[0], 1] == st)
-        
-    st_idx = st_idx[0]
+        st_idx = np.where(table[Z_idx[0], 1] == st)    
+    st_idx = st_idx[0]  # before that, st_idx is a tuple of a single array
     
     # turning Z, st and s into their corresponding names (for a more detailled graph)
     element, stage, gap =  correspondence(Z, st, s)
@@ -64,11 +62,10 @@ def electrons(Z, st, s):
     plt.legend()
     plt.xlabel("Number of emitted electrons")
     plt.ylabel("Probability")
-
     plt.savefig('graph_' + element + '_' + stage + '_' + gap + '.png')
-    
+
 """
-# examples : Fe I and Cr XII :
+# examples : Fe I and Cr XII
 electrons(26, 1, 1)   # Fe I
 electrons(24, 12, 3)    # Cr XII
 """
@@ -77,33 +74,33 @@ def all_electrons(S) :  # the only variable is the intial shell vacancy. Three c
     
     electrons_nb = []
     Z_elec = []
-    for z in range(4,30) :  # iterating for each atom (Z: 4-30)
+    for z in range(4,30) :  # iterating for each neutral atom (Z: 4-30)
         Z_idx = np.where(table[:, 0] == z)
         Z_idx = Z_idx[0]
         
-        average = 0
-        avg_all = []
-
-
-        gap = correspondence(0, 0, S)[2]    # the only relevant data is the gap type
-        avg_all = table[Z_idx[0] + S-1, 6:]
+        average = 0 # average number of electrons for a specific Z and inner shell vacancy
+        proba = table[Z_idx[0] + S-1, 6:] # probability to get X electrons
         if table[Z_idx[0] + S-1, 1] == 1 and table[Z_idx[0] + S-1, 0] == z: # to make sure the right ionisation stage exists for this atom
-            for i in range(len(avg_all)) :
-                average += avg_all[i]*(i+1)
+            for i in range(len(proba)) :
+                average += proba[i]*(i+1)
         
         Z_elec = np.append(Z_elec, average)
-    electrons_nb = np.append(electrons_nb, Z_elec)/10000
+    electrons_nb = np.append(electrons_nb, Z_elec)/10000    # divided by 10000
     
+
+    # Plotting the graph
+    gap = correspondence(0, 0, S)[2]    # the only relevant data is the gap type
     x = np.arange(5,31)
-    plt.plot(x, electrons_nb , drawstyle = 'steps', label = gap + "-shell")
+    plt.plot(x, electrons_nb , drawstyle = 'steps', label = gap + "-shell vacancy")
     plt.title("Average number of electrons emitted during the decay of an inner-shell vacancy")
     plt.legend()
     plt.xlabel("Atomic number")
     plt.ylabel("Number of electrons")
+    plt.savefig("nb_of_electrons_" + gap + "-shell.png")
     return(electrons_nb)
 
 
-all_electrons(1), all_electrons(2), all_electrons(3)    # graphs for the K, L_1 and M_1 shell vacancies
+all_electrons(1), all_electrons(2), all_electrons(5)    # graphs for the K, L_1 and M_1 shell vacancies
 
 
 
