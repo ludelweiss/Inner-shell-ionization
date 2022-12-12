6183d47d0d07e2ff7732e24f8d7c3e66c15a6184
Lud <aludmilla@orange.fr>
2022-12-12 14:23:24 +0100

    Changes to be committed: 	modified:   avg_photon.py

--- avg_photon.py
+++ avg_photon.py
@@ -29,12 +29,28 @@ def energy_Z(Z, s):
         E_e = tab[Z_idx[0]+s_idx[0], 4]
         E_p = tab[Z_idx[0]+s_idx[0], 6]
     
-    plt.plot(N_e, E_e, label="electrons")
-    plt.plot(N_e, E_p, label="photons")
-    plt.title("Element")
+    zipped_e = zip(N_e, E_e)
+    sorted_e = sorted(zipped_e)
+    tuples_e = zip(*sorted_e)
+    N_e, E_e =  [list(tuple) for tuple in tuples_e]
+    
+    zipped_p = zip(N_e, E_p)
+    sorted_p = sorted(zipped_p)
+    tuples_p = zip(*sorted_p)
+    N_e, E_p =  [list(tuple) for tuple in tuples_p]
+    
+    plt.plot(N_e, E_e, drawstyle="steps",label="electrons")
+    plt.plot(N_e, E_p, drawstyle="steps",label="photons")
+    plt.title("Z="+str(Z)+" and s="+str(s))
     plt.xlabel("Number of Auger electrons")
     plt.ylabel("Energy (eV)")
     plt.legend()
     return(N_e, E_e, E_p)
     
-A = energy_Z(8, 1)
+A = energy_Z(17, 1)
+
+"""
+plt.figure()
+plt.plot(tab[:,3], tab[:,4], drawstyle="steps",label="electrons")
+plt.plot(tab[:,3], tab[:,6], drawstyle="steps", label="photons")
+"""
